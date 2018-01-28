6595bf33a1e1a4e8f82bac3097de458c0fb91e2c
Victor Golovnin <victor.golovnin@gmail.com>
2018-01-29 01:20:51 +0300

    login v2

--- front/pages/login.vue
+++ front/pages/login.vue
@@ -1,8 +1,7 @@
 <template>
 	<div>
-    Добро пожаловать в библиотеку ЦЭКИ!
-  
   <div class="login-form">
+    <h1>Добро пожаловать в библиотеку ЦЭКИ!</h1>
     <el-input placeholder="Email" v-model="auth.email"></el-input>
     <el-input placeholder="Пароль" type="password" v-model="auth.password"></el-input>
     <el-button @click="tryAuth">Войти</el-button>
@@ -30,7 +29,21 @@
 
 <style lang="scss">
   .login-form {
+    display: flex;
+    flex-direction: column;
+    justify-content: space-evenly;
+
+    height: 300px;
+    max-width: 800px;
+
     background-color: rgba(54, 55, 88, 0.4);
     padding: 5em;
+
+    margin: auto;
+    text-align: center;
+
+    h1 {
+      color: white;
+    }
   } 
 </style>
