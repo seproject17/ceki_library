60a6183774ce46c71e7b5984fde5d94b9c0e8f01
Victor Golovnin <victor.golovnin@gmail.com>
2018-01-28 21:52:33 +0300

    add spaces

--- api/config/routes.rb
+++ api/config/routes.rb
@@ -1,13 +1,15 @@
 Rails.application.routes.draw do
-  resources :reviews
-  resources :books do
-    get '/books/:id/items', action: :find_items
-    get '/books/:id/items/free', action: :find_free_items
-    get '/users/:id/books/read', action: :find_read_books
-    get '/users/:id/books/borrowed', action: :find_borrowed_books
+  namespace :api do
+    resources :reviews
+    resources :books do
+      get '/books/:id/items', action: :find_items
+      get '/books/:id/items/free', action: :find_free_items
+      get '/users/:id/books/read', action: :find_read_books
+      get '/users/:id/books/borrowed', action: :find_borrowed_books
+    end
+    resources :users
+    post 'login' => 'users#login'
+    put 'change_password' => 'users#change_password'
+    put 'change_email' => 'users#change_email'
   end
-  resources :users
-  post 'login' => 'users#login'
-  put 'change_password' => 'users#change_password'
-  put 'change_email' => 'users#change_email'
 end
