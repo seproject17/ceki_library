41a10149414a6866be3595f772a9fa7d9d22cfe3
Victor Golovnin <victor.golovnin@gmail.com>
2018-01-28 21:42:42 +0300

    update db migrate

--- api/db/schema.rb
+++ api/db/schema.rb
@@ -10,67 +10,64 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171219003854) do
+ActiveRecord::Schema.define(version: 20180128215955) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension 'plpgsql'
+  enable_extension "plpgsql"
 
-  create_table 'book_items', force: :cascade do |t|
-    t.string 'file'
-    t.integer 'pages'
-    t.string 'location'
-    t.string 'type'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "book_items", force: :cascade do |t|
+    t.string "file"
+    t.integer "pages"
+    t.string "location"
+    t.string "type"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'book_operations', force: :cascade do |t|
-    t.string 'type'
-    t.date 'start_date'
-    t.date 'end_date'
-    t.string 'comments'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.bigint 'book_item_id'
-    t.index ['book_item_id'], name: 'index_book_operations_on_book_item_id'
-    t.index ['user_id'], name: 'index_book_operations_on_user_id'
+  create_table "book_operations", force: :cascade do |t|
+    t.string "type"
+    t.date "start_date"
+    t.date "end_date"
+    t.string "comments"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.bigint "book_item_id"
+    t.index ["book_item_id"], name: "index_book_operations_on_book_item_id"
+    t.index ["user_id"], name: "index_book_operations_on_user_id"
   end
 
-  create_table 'books', force: :cascade do |t|
-    t.string 'isbn'
-    t.string 'title'
-    t.string 'author'
-    t.string 'publisher'
-    t.date 'year'
-    t.string 'annotations'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "books", force: :cascade do |t|
+    t.string "isbn"
+    t.string "title"
+    t.string "author"
+    t.string "publisher"
+    t.date "year"
+    t.string "annotations"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  create_table 'reviews', force: :cascade do |t|
-    t.string 'comments'
-    t.integer 'mark'
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
-    t.bigint 'user_id'
-    t.bigint 'book_id'
-    t.index ['book_id'], name: 'index_reviews_on_book_id'
-    t.index ['user_id'], name: 'index_reviews_on_user_id'
+  create_table "reviews", force: :cascade do |t|
+    t.string "comments"
+    t.integer "mark"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_reviews_on_user_id"
   end
 
-  create_table 'users', force: :cascade do |t|
-    t.string 'name'
-    t.string 'surname'
-    t.string 'email'
-    t.string 'password_digest'
-    t.integer 'role', default: 0, null: false
-    t.datetime 'created_at', null: false
-    t.datetime 'updated_at', null: false
+  create_table "users", force: :cascade do |t|
+    t.string "name"
+    t.string "surname"
+    t.string "email"
+    t.string "password_digest"
+    t.integer "role", default: 0, null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
   end
 
-  add_foreign_key 'book_operations', 'book_items'
-  add_foreign_key 'book_operations', 'users'
-  add_foreign_key 'reviews', 'books'
-  add_foreign_key 'reviews', 'users'
+  add_foreign_key "book_operations", "book_items"
+  add_foreign_key "book_operations", "users"
+  add_foreign_key "reviews", "users"
 end
