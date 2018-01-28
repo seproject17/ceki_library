b7df5c5430ae85e097288ba6f1273dc1e17d2bfa
Victor Golovnin <victor.golovnin@gmail.com>
2018-01-29 00:51:59 +0300

    Auto stash before merge of "dev" and "origin/dev"

--- front/pages/login.vue
+++ front/pages/login.vue
@@ -1,8 +1,36 @@
 <template>
-	
+	<div>
+    Добро пожалвать в библиотеку ЦЭКИ!
+  
+  <div class="login-form">
+    <el-input placeholder="Email" v-model="auth.email"></el-input>
+    <el-input placeholder="Пароль" type="password" v-model="auth.password"></el-input>
+    <el-button @click="tryAuth">Войти</el-button>
+  </div>
+  </div>
 </template>
 <script>
 	export default {
-
+    data() {
+      return {
+        auth: {
+          email: '',
+          password: ''
+        }
+      }
+    },
+    methods: {
+      async tryAuth() {
+        let resp = await this.$axios.post('/login')
+        console.log(resp)
+      }
+    }
 	}
 </script>
+
+<style lang="scss">
+  .login-form {
+    background-color: rgba(54, 55, 88, 0.4);
+    padding: 5em;
+  } 
+</style>
