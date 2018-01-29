45427b0d664977855cf615dfdf39c1e5958c5d56
Unknown <sapozhkov2008@yandex.ru>
2018-01-29 03:03:25 +0300

    index.vue  template ready

--- front/pages/index.vue
+++ front/pages/index.vue
@@ -1,3 +1,76 @@
 <template>
-	<div>NAVALNY PIDOR</div>
+    <div class="books-page-container">
+        <el-input placeholder="Введите название книги" v-model="searchField">
+            <template slot="append">
+                <el-button>Поиск</el-button>
+            </template>
+        </el-input>
+        <div class="books-container">
+            <nuxt-link to="{ name: 'book' }" class="book-item" v-for="book in books" :key="book.id">
+                <div class="book-image"></div>
+                <p>
+                    {{book.title}}
+                </p>
+                <p>
+                    {{book.author}}
+                </p>
+            </nuxt-link>
+        </div>
+    </div>
 </template>
+<style lang="scss">
+    .books-page-container {
+        font-size: 1px;
+        max-width: 1200px;
+        margin: auto;
+    }
+
+    .books-container {
+        display: flex;
+        font-size: 17em;
+        justify-content: center;
+        flex-wrap: wrap;
+        .book-item {
+            background-color: red;
+            flex-basis: 20%;
+            margin: 10px;
+            .book-image {
+                height: 200px;
+                width: 150px;
+                margin: auto;
+                background: url(/book.png) center center;
+                background-size: contain;
+                background-repeat: no-repeat;
+            }
+
+        }
+        p {
+            text-align: center;
+            color: white;
+            text-decoration: none;
+        }
+    }
+</style>
+<script>
+export default {
+    created() {
+        this.$axios.$get('/books').then(res => {
+            this.books = res;
+        });
+    },
+    data() {
+        return {
+            books: [],
+            err: null
+        };
+    },
+    methods: {
+        bug() {
+            this.$axios.get('/users').then(res => {
+                console.log('LLLL', res);
+            });
+
+        }
+    }
+};
+</script>
