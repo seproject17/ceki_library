fbe522f99920d7655fd18c986029d61f3b08694a
Roman Baygildin <rbaygildin95@gmail.com>
2018-03-18 00:39:02 +0300

    Render review with users and books

--- api/app/models/review.rb
+++ api/app/models/review.rb
@@ -2,9 +2,9 @@ class Review < ApplicationRecord
   belongs_to :user, optional: false
   belongs_to :book, optional: false
 
-  # def as_json(options = {})
-  #   @user = self.user.as_json
-  #   @book = self.book.as_json
-  #   super((options || {})).merge({'user': @user, 'book': @book})
-  # end
+  def as_json(options = {})
+    @user = self.user.as_json
+    @book = self.book.as_json
+    super((options || {})).merge({'user': @user, 'book': @book})
+  end
 end
