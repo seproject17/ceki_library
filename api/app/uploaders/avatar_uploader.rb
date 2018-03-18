a7b989362f5837bd768f9b815d18585f7d9b2d4f
Roman Baygildin <rbaygildin95@gmail.com>
2018-03-18 04:49:28 +0300

    Resolved 500

--- api/app/uploaders/avatar_uploader.rb
+++ api/app/uploaders/avatar_uploader.rb
@@ -42,6 +42,10 @@ class AvatarUploader < CarrierWave::Uploader::Base
   # Avoid using model.id or version_name here, see uploader/store.rb for details.
   def filename
     # "something.jpg" if original_filename
-    SecureRandom.uuid.gsub('-', '') + '.' + file.extension
+    if file
+      SecureRandom.uuid.gsub('-', '') + '.' + file.extension
+    else
+      super
+    end
   end
 end
